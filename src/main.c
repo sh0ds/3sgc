f8649ecd49a6de0b6f8ab3900e1b6a18f7524104
sh0ds <mail@sh0ds.dev>
2026-09-22 21:32:00 +0200

    basic file reading

--- src/main.c
+++ src/main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char* argv[]) {
-    if (argc > 1) {
+void copy_file(const char *src, const char *dst) {
+    FILE *f_src = fopen(src, "rb");
+    FILE *f_dst = fopen(dst, "wb");
+    char str[100];
+    while (fgets(str, 100, f_src)) {
+        printf("%s", str);
+    }
+}
+
+int main (/*int argc, char *argv[]*/) {
+    /*if (argc > 1) {
         printf("Building %s\n", argv[1]);
     } else {
         printf("usage: statiq path_to_site_folder\n");
         exit(1);
-    }
+        }*/
+
+    copy_file("./tests/site1/static/style.css", "./tmp/style.css");
 
     return 0;
 }
