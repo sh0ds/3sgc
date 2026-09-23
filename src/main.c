82cfa28691ad0224b3e3f04219bfa1055d2150df
Tizian <mail@sh0ds.dev>
2026-09-23 11:54:26 +0000

    commenting

--- src/main.c
+++ src/main.c
@@ -2,21 +2,26 @@
 #include <stdlib.h>
 #include <dirent.h>
 
+// copies src to dst, in buffer sizes of 4KB
 int copy_file(const char *src, const char *dst) {
     FILE *f_src, *f_dst;
 	
+	// open source file
 	f_src = fopen(src, "rb");
 	if (f_src == NULL) {
 		perror("Error opening source file");
 		return -1;
 	}
 
+	// open destination file
     f_dst = fopen(dst, "wb");
 	if (f_dst == NULL) {
 		perror("Error opening destination file");
 		fclose(f_src);
 		return -1;
 	}
+
+	// while fread returns > 0, write to destination
     char buf[4096];
     size_t rf;
 	while ((rf = fread(buf, 1, sizeof buf, f_src)) > 0) {	
@@ -34,23 +39,26 @@ int copy_file(const char *src, const char *dst) {
 		fclose(f_dst);
 		return -1;
 	}
+
 	fclose(f_src);
 	fclose(f_dst);
 	
 	return 0;
 }
 
-
+// open folder and print everything in it
 int print_folder(const char* folder){ 
 	DIR *dir;
 	struct dirent *entry;
 
+	// open folder
 	dir = opendir(folder);
 	if (dir == NULL) {
 		perror("Cannot open folder");
 		return -1;
 	}
 
+	// print every entry
 	while ((entry = readdir(dir)) != NULL) {
 		printf("%s\n", entry->d_name);
 	}
